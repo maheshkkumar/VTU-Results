62dabf54fab3fbba60dc2826292f60e2142be7a2
Mahesh Kumar K <maheshkumar.wdp21@gmail.com>
2015-07-13 16:08:05 +0530

    added multiple results facility

--- vtu_result.py
+++ vtu_result.py
@@ -1,24 +1,40 @@
 #!/usr/bin/python   
 # -*- coding: utf-8 -*-
-#######################################  
-# Created by Mahesh Kumar K           #
-# Python Script to fetch VTU Results  #
-#######################################
+######################################  
+# Created by Mahesh Kumar K          #
+# Python Script to fetch VTU Result  #
+######################################
 
 import requests
 from lxml import html
 
 base_url = 'http://results.vtu.ac.in/vitavi.php?rid='
-
+usn = []
 def rolno():
-    usn = raw_input('Enter your usn: ')
-    if len(usn) == 10:
-        complete_url = base_url+usn+'&submit=SUBMIT'
-        response = requests.get(complete_url)
-        text = html.fromstring(response.text)
-        print_marks(text)    
-    else :
-        print "Invalid USN"
+    choice = input("Press 1 for single result and 2 for multiple results : ")
+    if choice==1:
+        s_usn = raw_input('Enter your usn : ')
+        if len(s_usn) == 10:
+            complete_url = base_url+s_usn+'&submit=SUBMIT'
+            response = requests.get(complete_url)
+            text = html.fromstring(response.text)
+            print_marks(text)    
+        else :
+            print "Invalid USN"
+    else:
+       num = input("Enter the number of results, that you want to get : ")
+       for i in range(0, num):
+           s_usn = raw_input('Enter your usn : ')
+           usn.append(s_usn)
+           print usn
+       for i in range(0,num):
+           if len(usn[i]) == 10:
+               complete_url = base_url+usn[i]+'&submit=SUBMIT'
+               response = requests.get(complete_url)
+               text = html.fromstring(response.text)
+               print_marks(text)    
+           else :
+               print "Invalid USN"
 
 def print_marks(html_data):
     subjects = html_data.xpath('//i/text()')
@@ -29,13 +45,14 @@ def print_marks(html_data):
     student_name = student_details[0]
     semester = int(student_details[2])
     result = student_details[3].encode('ascii','ignore')
+    print "******************************************************"
     print "Name : "+student_name
     print "Semester: ",semester
     print "Marks format is : Subject Name / Internal / External / Total"
     if subjects:
         internal = marks[5::3]
         external = marks[4::3]
-        total = marks[6::3]        
+        total = marks[6::3]     
         for i,j,k,l in zip(subjects, internal, external, total):
             print i,j,k,l
         print "Total Marks = ",total_marks
@@ -45,7 +62,8 @@ def print_marks(html_data):
             print "Average = ",round(float(total_marks * 100)/775, 2)
         else:
             print "Average = ",round(float(total_marks * 100)/900, 2)
-        print "Result = "+result[8:]    
+        print "Result = "+result[8:]   
+        print "******************************************************"
     else:
         print "Enter a Valid USN"
     
